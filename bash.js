9dab029f7fdb1b9dbdea1572d3d84ab72f8d2f18
Ksenia Mikhailovskaya <kseniamikhailovskaya@Ksenias-MacBook-Pro.local>
2016-01-19 19:45:13 -0500

    Commands.js up to echo function

--- bash.js
+++ bash.js
@@ -1,19 +1,35 @@
-//console.log(process.argv[2])
-
 var commands = require('./commands.js')
 var fs = require('fs');
-
+var request = require('request');
 
 //Output a prompt
 process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function(data) {
-  var cmd = data.toString().trim(); // remove the newline
+
+  var cmd = data.toString().trim();
   var splitData = cmd.split(" ");
+
+  var splitByPipe = cmd.split(/\s*\|\s*/g)
   var command = splitData[0];
-  var file = splitData[1];
-  commands[command](file);
+  var file = splitData[1]; //by ' '
+  var next = splitByPipe[1]; // by '|' portion after pipe
+  //console.log("1",splitData[0],"2",splitData[1],"3",splitByPipe[1]);
+  commands[command](undefined, file, done);
+
+  function done(output) {
+  	if(next) {
+  		var n = next;
+  		next = undefined;
+  		commands[n](output, undefined, done);
+  	} else {
+  		process.stdout.write(output);
+		process.stdout.write('\nprompt > '); 
+  	}
+
+}
+
 
 });
 
--- commands.js
+++ commands.js
@@ -1,70 +1,96 @@
-
+//var bash = require('./bash.js')
 var fs = require('fs');
 
-exports.pwd = function(file) {
-	process.stdout.write(process.cwd());
-  	process.stdout.write('\nprompt > '); 
+var request = require('request');
 
+exports.pwd = function(stdin,file,done) {
+
+ 	if(stdin === undefined) {
+ 		done(process.cwd());
+ 	} 
+	
  }
 
- exports.date = function(file) {
+ exports.date = function(stdin,file,done) {
     	var newDate = new Date;
-  		process.stdout.write(newDate.toString());
-  		process.stdout.write('\nprompt > '); 
+  		done(newDate.toString());
  }
 
-exports.ls = function(file) {
+exports.ls = function(stdin,file,done) {
 
 	fs.readdir('.', function(err, files) {
+		var outputString = "";
   		if (err) throw err;
   		files.forEach(function(file) {
-    		process.stdout.write(file.toString() + "\n");
-    		process.stdout.write("prompt > ");
+    		outputString += (file.toString() + "\n");
   		})
-  		
+  		done(outputString);
 	});
 };
 
-exports.echo = function(file) {
-	console.log(file);
-	process.stdout.write('\nprompt > ');
+exports.echo = function(stdin,file,done) {
+	done(file);
 }
 
-exports.cat = function(file) {
-	fs.readFile(file, (err, data) => {
-  		if (err) throw err;
-  			process.stdout.write(data);
-  			process.stdout.write('\nprompt > ');
-	});
-	
+exports.cat = function(stdin,file,done) {
+	if(file) {
+		fs.readFile(file, (err, data) => {
+	  		if (err) throw err;
+	  			done(data);
+		});		
+	} else {
+		done(stdin);
+	}
 }
 
 
-exports.head = function(file) {
-	fs.readFile(file, 'utf8', (err, data) => {
-	  			if (err) throw err;
-	  			var count = 1;
-	  			data = data.toString();
+exports.head = function(stdin,file,done) {
+	if(file) {
+		fs.readFile(file, 'utf8', (err, data) => {
+			if (err) throw err;
+			var count = 1;
+			data = data.toString();
 
-	  			var slicePosition = 0;
+			var slicePosition = 0;
 
-	  			for(var i = 0; i < data.length; i++) {
-	  				if(data[i] === '\n') {
-	  					count++;
-	  				}
-	  				if(count === 6) {
-	  					slicePosition = i;
-	  					break;
-	  				}
-	  			}
+			for(var i = 0; i < data.length; i++) {
+				if(data[i] === '\n') {
+					count++;
+				}
+				if(count === 6) {
+					slicePosition = i;
+					break;
+				}
+			}
 
-	  			var outputString = data.slice(0,slicePosition);
-	  				process.stdout.write(outputString);
-				  	process.stdout.write('\nprompt > ');
-				});
+			var outputString = data.slice(0,slicePosition);
+				done(outputString);
+		});
+
+	} else {
+
+			var count = 1;
+			stdin = stdin.toString();
+
+			var slicePosition = 0;
+
+			for(var i = 0; i < stdin.length; i++) {
+				if(stdin[i] === '\n') {
+					count++;
+				}
+				if(count === 6) {
+					slicePosition = i;
+					break;
+				}
+			}
+
+			var outputString = stdin.slice(0,slicePosition);
+				done(outputString);
+		
+	}
 }
 
-exports.tail = function(file) {
+exports.tail = function(stdin,file,done) {
 	fs.readFile(file, 'utf8', (err, data) => {
 	  			if (err) throw err;
 	  			var count = 1;
@@ -83,13 +109,12 @@ exports.tail = function(file) {
 
 	  			var outputString = ''
 	  			outputString += data.slice(slicePosition);
-	  			process.stdout.write(outputString);
-				process.stdout.write('\nprompt > ');
+	  			bash.done(outputString);
 				});
 }
 
 
-exports.wc = function(file) {
+exports.wc = function(stdin,file,done) {
 	fs.readFile(file, 'utf8', (err, data) => {
 		if (err) throw err;
 		var count = 1;
@@ -100,12 +125,11 @@ exports.wc = function(file) {
 			}
 		}
 
-		process.stdout.write(count.toString());
-	process.stdout.write('\nprompt > ');
+		bash.done(count.toString());
 	});
 }
 
-exports.sort = function(file) {
+exports.sort = function(stdin,file,done) {
 	fs.readFile(file, 'utf8', (err, data) => {
 		if (err) throw err;
 		data = data.toString();
@@ -113,17 +137,45 @@ exports.sort = function(file) {
 		var dataArr = data.split('\n');
 		var outputString = dataArr.sort().join('\n');
 
-		process.stdout.write(outputString);
-		process.stdout.write('\nprompt > ');
+		bash.done(outputString);
+
 	});
 }
 
+exports.uniq = function(stdin,file,done) {
+	fs.readFile(file, 'utf8', (err, data) => {
+		if (err) throw err;
+		data = data.toString();
+		
+		var dataArr = data.split('\n').sort();
+
+		for(var i = 0; i < dataArr.length; i++) {
+	    	if(dataArr[i] === dataArr[i+1]) {
+	        	dataArr = dataArr.slice(0, i).concat(dataArr.slice(i+1));
+	    	}
+	
+		}
+
+		var outputString = dataArr.join('\n');
 
 
+		bash.done(outputString);
+	});
+
+}
 
 
+/*
+Now that you have request available to require, implement the curl command. It should make an HTTP GET request to a given URL, and print out the HTTP response body.
+*/
 
-
+exports.curl = function(webAddress) {
+	request(webAddress, function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	    bash.done(body);
+	  }
+	});
+}
 
 
 
