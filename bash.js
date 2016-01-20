06a9fae8ff3d13a0bbe78d4dc9d0b5ac1d3dc360
Ksenia Mikhailovskaya <ksenia0822@gmail.com>
2016-01-20 09:47:50 -0500

    Renamed next to secondCommand

--- bash.js
+++ bash.js
@@ -9,19 +9,24 @@ process.stdout.write('prompt > ');
 process.stdin.on('data', function(data) {
 
   var cmd = data.toString().trim();
+
+  //Spliting user input by white space
   var splitData = cmd.split(" ");
 
+  //Spliting user input by white space
   var splitByPipe = cmd.split(/\s*\|\s*/g)
+
   var command = splitData[0];
-  var file = splitData[1]; //by ' '
-  var next = splitByPipe[1]; // by '|' portion after pipe
+  var file = splitData[1]; 
+
+  var secondCommand = splitByPipe[1]; 
   //console.log("1",splitData[0],"2",splitData[1],"3",splitByPipe[1]);
   commands[command](undefined, file, done);
 
   function done(output) {
-  	if(next) {
-  		var n = next;
-  		next = undefined;
+  	if(secondCommand) {
+  		var n = secondCommand;
+  		secondCommand = undefined;
   		commands[n](output, undefined, done);
   	} else {
   		process.stdout.write(output);
